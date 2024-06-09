d95db17f9877ff07116f623603c5cca43e308b90
Samuel Holmes <samuel.holmes-ext@pole-emploi.fr>
2024-06-09 15:04:04 +0200

    add env variable and more nav

--- frontend/src/components/ListContainer.vue
+++ frontend/src/components/ListContainer.vue
@@ -35,16 +35,24 @@
     </div>
 
     <!-- List Section -->
-    <div class="list-section">
-      <router-link
+    <div
         v-for="item in items"
         :key="item._id"
-        :to="`/list/${item._id}`"
         class="list-item"
       >
-        {{ item.name }}
-      </router-link>
-    </div>
+        <router-link
+          :to="`/lists/${item._id}`"
+          class="list-link"
+        >
+          {{ item.name }}
+        </router-link>
+        <button @click="removeItem(item._id)" class="trash-button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+          </svg>
+        </button>
+      </div>
   </div>
 </template>
   
@@ -65,6 +73,7 @@
       const hashEmail = (email: string) => {
         return CryptoJS.SHA256(email).toString(CryptoJS.enc.Hex);
       };
+      const backendUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 
       const { user, isAuthenticated, isLoading, logout } = useAuth0();
       let hashedEmail = "";
@@ -72,27 +81,27 @@
       const newItem = ref<GroceryList>({ _id: '', name: ''});
   
       const fetchList = async () => {
-        const response = await axios.get(`http://localhost:8000/user/${hashedEmail}/grocery-list`);
+        const response = await axios.get(`${backendUrl}` + '/user/'+ `${hashedEmail}` + '/grocery-list');
         items.value = response.data;
       };
   
       const joinList = async () => {
         if (newItem.value._id !== "") {
-          await axios.patch(`http://localhost:8000/grocery-list/${newItem.value._id}/user/${hashedEmail}`);
+          await axios.patch(`${backendUrl}/grocery-list/${newItem.value._id}/user/${hashedEmail}`);
           fetchList();
         }
       }
   
       const addItem = async () => {
         if (newItem.value.name !== "") {
-          await axios.post(`http://localhost:8000/user/${hashedEmail}/grocery-list`, newItem.value);
+          await axios.post(`${backendUrl}/user/${hashedEmail}/grocery-list`, newItem.value);
           newItem.value.name = '';
           fetchList();
         }
       };
   
-      const removeItem = async (name: string) => {
-        await axios.delete(`http://localhost:8000/grocery/${name}`);
+      const removeItem = async (id: string) => {
+        await axios.delete(`${backendUrl}/grocery-list/${id}`);
         fetchList();
       };
 
@@ -176,18 +185,35 @@
 }
 
 .list-item {
-  display: block;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   padding: 8px;
   background-color: #f8f9fa;
   color: #343a40;
   border: 1px solid #dee2e6;
   border-radius: 4px;
   margin-bottom: 8px;
-  text-decoration: none;
 }
 
-.list-item:hover {
-  background-color: #e2e6ea;
+.list-link {
+  flex-grow: 1;
+  text-decoration: none;
+  color: inherit;
+}
+
+.trash-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+}
+
+.trash-button:hover svg {
+  fill: red;
 }
 </style>
   
