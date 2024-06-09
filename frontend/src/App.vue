b58d8a8fbc9cbfebb88996adda8ae58ee37919d5
Samuel Holmes <samuel.holmes-ext@pole-emploi.fr>
2024-06-09 08:47:47 +0200

    style and content

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" class="d-flex flex-column h-100">
+  <div id="app" class="flex flex-col min-h-screen">
     <nav-bar />
-    <div class="container flex-grow-1">
+    <div class="container flex-grow">
       <error />
       <div class="mt-5">
         <router-view />
@@ -10,8 +10,7 @@
     <footer class="bg-light text-center p-3">
       <div class="logo"></div>
       <p>
-        Sample project provided by
-        <a href="https://auth0.com">Auth0</a>
+        What do we eat ?
       </p>
     </footer>
   </div>
@@ -28,3 +27,13 @@ export default {
   }
 };
 </script>
+<style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
