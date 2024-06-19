a6d3584662a0b926c1c82159054404ead363eeb9
Samuel Holmes <samuel.holmes-ext@pole-emploi.fr>
2024-06-19 16:55:23 +0200

    implement pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(), 
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Whatdoweeat',
+        short_name: 'Whatdoweeat',
+        description: 'Find out what you are eating',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+              "src": "/android-chrome-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png"
+          }
+        ]
+      }
+     })],
+});
